801aba2e37852f97d1ac6a47a12531fde5a02232
Romain <leconteromain.lr@gmail.com>
2017-10-27 12:12:50 +0200

    add data for backoffice

--- routes/dash_boards.js
+++ routes/dash_boards.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var listBoards  = [
+  { picto: "aspect_ratio", categorie: "Board1", content: "" },
+  { picto: "aspect_ratio", categorie: "Board2", content: "" },
+  { picto: "aspect_ratio", categorie: "Board3", content: ""}
+];
+
+var articles  = [
+  { image: "./images/sample-1.jpg", title: 'Titre article', description: 'Here is some more information about this product that is only revealed once clicked on.' },
+  { image: "./images/sample-1.jpg", title: 'Titre', description: 'Here is some more information about this product.' },
+  { image: "./images/sample-1.jpg", title: 'Titre d\'article', description: 'Here is some more.' },
+  { image: "./images/sample-1.jpg", title: 'Titre article', description: 'Here is some more information about this product that is only.' },
+  { image: "./images/sample-1.jpg", title: 'Titre new article', description: 'Here is some more information about this product that is only revealed once clicked on.' },
+  { image: "./images/sample-1.jpg", title: 'Titre article', description: 'Here is some more information about this.' },
+  { image: "./images/sample-1.jpg", title: 'Titre article', description: 'Here is some more information about this product that is only revealed once clicked on.' },
+];
+
+JSON.stringify(listBoards, articles);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dash_boards', { title: 'Boards', page_name: 'boards', role_user: 'Admin' });
+  res.render('dash_boards', { title: 'Boards',articles: articles, listBoards: listBoards , page_name: 'boards', role_user: 'Admin' });
 });
 
 module.exports = router;
--- routes/dash_users.js
+++ routes/dash_users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var Users  = [
+  { id: "001", firstName: "Tiago", lastName: "Arti", dateInscription: "12/03/2010", boards : "22", images : "100", status : "online" , icon: "fiber_manual_record" },
+  { id: "002", firstName: "Carlos", lastName: "Malo", dateInscription: "12/03/2010", boards : "22", images : "100", status : "offline" , icon: "do_not_disturb_alt" },
+  { id: "003", firstName: "Victor", lastName: "Furtu", dateInscription: "12/03/2010", boards : "22", images : "100", status : "online" , icon: "fiber_manual_record" },
+  { id: "004", firstName: "Robnan", lastName: "Resh", dateInscription: "12/03/2010", boards : "22", images : "100", status : "offline" , icon: "do_not_disturb_alt" },
+  { id: "005", firstName: "Romain", lastName: "Fureur", dateInscription: "12/03/2010", boards : "22", images : "100", status : "offline" , icon: "do_not_disturb_alt" },
+  { id: "006", firstName: "Maxime", lastName: "Tisti", dateInscription: "12/03/2010", boards : "22", images : "100", status : "online" , icon: "fiber_manual_record" },
+  { id: "007", firstName: "Marie-Claire", lastName: "Junus", dateInscription: "12/03/2010", boards : "22", images : "100", status : "offline" , icon: "do_not_disturb_alt" },
+  { id: "008", firstName: "Justine", lastName: "Fost", dateInscription: "12/03/2010", boards : "22", images : "100", status : "online" , icon: "fiber_manual_record" },
+  { id: "009", firstName: "Fabienne", lastName: "Carlis", dateInscription: "12/03/2010", boards : "22", images : "100", status : "offline" , icon: "do_not_disturb_alt" }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dash_users', { title: 'Users', page_name: 'users', role_user: 'Admin' });
+  res.render('dash_users', { title: 'Users',Users : Users, page_name: 'users', role_user: 'Admin' });
 });
 
 module.exports = router;
