787acbff6af34ae35f513d86fcb1320e2e84ea60
Romain <leconteromain.lr@gmail.com>
2017-10-29 16:33:53 +0100

    add token connection

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 
 var index = require('./routes/index');
 var profile = require('./routes/profile');
 var boards = require('./routes/boards');
 var inscription = require('./routes/inscription');
 var login = require('./routes/login');
-var forget = require('./routes/forget');
+var forget = require('./routes/forget'); 
 
 var dashboard = require('./routes/dashboard');
 var dash_users = require('./routes/dash_users');
@@ -27,7 +28,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 const fakeUser = {email: 'test@test.fr', password: 'test'};
+const secret = 'YUN2&-4(-FBVdfz&h_C(4D6(-__46yrtyty5y';
 
 /* POST home page. */
 router.post('/', function(req, res, next) {
@@ -15,12 +17,13 @@ router.post('/', function(req, res, next) {
    res.sendStatus(500);
  }else{
    if(fakeUser.email === req.body.email && fakeUser.password === req.body.password){
-     res.json({
-       test : 'heureux'
-     })
+    const token = jwt.sign({iss:'http://site.fr', user:'Sam', role:'user'}, secret);
+    res.json(token);
+   }else{
+     res.sendStatus(401);
    }
  }
   res.render('login', { title: 'Login' });
 });
 
-module.exports = router;  
+module.exports = router;
